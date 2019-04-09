fee4849a5c6947a6782aeb39ccd3659d35a4c7ed
Roy Shen <roythroyalboy@gmail.com>
2019-04-09 11:32:01 -0400

    Added edge cases for erase()
    This test throws an error for our current implementation, asking how the erase function should operate when substring doesn't exist.

--- pencil.cpp
+++ pencil.cpp
@@ -4,9 +4,17 @@ using namespace std;
 
 
 void write(string &paper, string &input) {
+	//simply append input onto paper
 	paper += input;
 }
 
 void erase(string &paper, string &input) {
-	cout << "erase " << input << " from " << paper;
+	//find the position of the last instance of input
+	size_t end = paper.find_last_of(input) + 1;
+	size_t start = end - input.length();
+	//replace this substring with space
+	while (start != end) {
+		paper.replace(start, size_t(1), " ");
+		start++;
+	}
 }
